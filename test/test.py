79608ffd1acb1678fa1d4328f724510908638193
Hazem Marar <H.marar@psut.edu.jo>
2026-04-01 21:53:29 +0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,37 +1,13 @@
-from cocotb.clock import Clock
 import cocotb
-from cocotb.triggers import RisingEdge
+from cocotb.triggers import Timer
 
 @cocotb.test()
-async def test_sbox(dut):
+async def test_all(dut):
 
-    # Start clock
-    clock = Clock(dut.clk, 10, unit="ns")
-    cocotb.start_soon(clock.start())
+    for i in range(10):
+        dut.ui_in.value = i
+        dut.uio_in.value = i + 1
 
-    # Apply reset
-    dut.rst_n.value = 0
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
+        await Timer(10, units="ns")
 
-    # Wait a few cycles in reset
-    for _ in range(3):
-        await RisingEdge(dut.clk)
-
-    # Release reset
-    dut.rst_n.value = 1
-
-    # Apply input
-    dut.ui_in.value = 0x53
-    dut.uio_in.value = 0b10  # mode = 10
-
-    # Wait for output to settle (VERY IMPORTANT)
-    for _ in range(3):
-        await RisingEdge(dut.clk)
-
-    # Now read output
-    result = dut.uo_out.value.to_unsigned()
-
-    print(f"Output: {result:#x}")
-
-    assert result == 0xED, f"Expected 0xED, got {result:#x}"
+        print(f"A={i}, B={i+1}, OUT={dut.uo_out.value}")
